d2dc2bac91601748254971c435bd26722349b77b
Om Prakash Singh <omprakashs855@gmail.com>
2026-01-07 23:12:35 +0530

    two sum adding brute force and two pass hash

--- solution/0001_two_sum.py
+++ solution/0001_two_sum.py
@@ -2,7 +2,38 @@
 
 from typing import List
 
-class Solution:
+# Brute Force Class
+class Solution_BruteForce:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        n = len(nums)
+
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
+        return []
+
+# Two Pass Hash Class
+class Solution_Two_Pass_Hash_Table:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        for i in range(n):
+            diff = target - nums[i]
+            if diff in numMap and numMap[diff] != i:
+                return [numMap[diff], i]
+
+        return []
+
+# One Pass Hash Table
+class Solution_One_Pass_Hash_Table:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         seen = {}
@@ -13,9 +44,15 @@ class Solution:
                 return [seen[val], idx]
             else:
                 seen[diff] = idx
+        return []
 
-        
-obj = Solution()
-obj.twoSum(nums = [2,7,11,15], target = 9)
+# obj1 = Solution_BruteForce()
+# print(obj1.twoSum(nums = [2,7,11,15], target = 9))
+
+# obj2 = Solution_Two_Pass_Hash_Table()
+# print(obj2.twoSum(nums = [2,7,11,15], target = 9))
+
+obj3 = Solution_One_Pass_Hash_Table()
+print(obj3.twoSum(nums = [2,7,11,15], target = 9))
 
 # To Run : python solution/0001_two_sum.py from root directory 
